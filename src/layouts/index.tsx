4b05a8ae2c78a3d230ecd7c3820daf714a800375
mee <1004460703@qq.com>
2023-05-06 17:50:58 +0800

    🐞 fix(layout): del none used import

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,9 +1,7 @@
-import { useSelector } from "react-redux";
 import LayoutClassic from "./LayoutClassic";
 import { ReactElement } from "react";
 import ThemeDrawer from "./components/Setting/components/ThemeDrawer";
 import { HotkeysProvider } from "@blueprintjs/core";
-import { RootState } from "../stores";
 import LayoutColumn from "./LayoutColumn";
 import LayoutTransverse from "./LayoutTransverse";
 import LayoutVertical from "./LayoutVertical";
